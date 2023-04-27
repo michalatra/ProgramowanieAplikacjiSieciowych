e412d44643d74f30f4c8600aa4ecec52579502cc
Michał Latra <michal.latra@gmail.com>
2023-04-28 01:22:25 +0200

    Lab 6 - solutions

--- Lab06/Zad10.py
+++ Lab06/Zad10.py
@@ -15,8 +15,10 @@ class SMTPCommand(Enum):
     MAIL = 2
     RCPT = 3
     DATA = 4
-    QUIT = 5
-    ERROR = 6
+    NOOP = 5
+    RSET = 6
+    QUIT = 7
+    ERROR = 8   
 
 
 class SMTPResponseCode(Enum):
@@ -24,11 +26,13 @@ class SMTPResponseCode(Enum):
     START_MAIL_INPUT = 2
     CLOSING_CONNECTION = 3
     SYNTAX_ERROR = 4
+    PARAMETER_ERROR = 5
 
 
 class SMTPResponse:
-    def __init__(self, command: SMTPCommand, response_code: SMTPResponseCode, response: bytes):
+    def __init__(self, command: SMTPCommand, data: str, response_code: SMTPResponseCode, response: bytes):
         self.command = command
+        self.data = data
         self.responseCode = response_code
         self.response = response
 
@@ -40,20 +44,98 @@ class Mail:
         self.content = content
 
 
-def interpret_command(command):
-    command = command.decode().split(" ")
-    if command[0] == "HELO":
-        return SMTPResponse(SMTPCommand.HELO, SMTPResponseCode.OK, b"250 OK\r\n")
-    elif command[0] == "MAIL":
-        return SMTPResponse(SMTPCommand.MAIL, SMTPResponseCode.OK, b"250 OK\r\n")
-    elif command[0] == "RCPT":
-        return SMTPResponse(SMTPCommand.RCPT, SMTPResponseCode.OK, b"250 OK\r\n")
-    elif command[0] == "DATA":
-        return SMTPResponse(SMTPCommand.DATA, SMTPResponseCode.START_MAIL_INPUT, b"354 Start mail input; end with <CRLF>.<CRLF>\r\n")
-    elif command[0] == "QUIT":
-        return SMTPResponse(SMTPCommand.QUIT, SMTPResponseCode.CLOSING_CONNECTION, b"221 Closing connection\r\n")
+def handle_helo_command(command: list[str]):
+    if len(command) != 2:
+        return SMTPResponse(SMTPCommand.ERROR, "", SMTPResponseCode.PARAMETER_ERROR, b"555 Parameter error, command unrecognized\r\n")
     else:
-        return SMTPResponse(SMTPCommand.ERROR, SMTPResponseCode.SYNTAX_ERROR, b"500 Syntax error, command unrecognized\r\n")
+        return SMTPResponse(SMTPCommand.HELO, command[1], SMTPResponseCode.OK, b"250 OK\r\n")
+    
+
+def handle_mail_command(command: list[str]):
+    if len(command) != 3 or command[1] != "FROM" or command[2] == "":
+        return SMTPResponse(SMTPCommand.ERROR, "", SMTPResponseCode.PARAMETER_ERROR, b"555 Parameter error, command unrecognized\r\n")
+    else:
+        return SMTPResponse(SMTPCommand.MAIL, command[1], SMTPResponseCode.OK, b"250 OK\r\n")
+    
+
+def handle_rcpt_command(command: list[str]):
+    if len(command) != 3 or command[1] != "TO" or command[2] == "":
+        return SMTPResponse(SMTPCommand.ERROR, "", SMTPResponseCode.PARAMETER_ERROR, b"555 Parameter error, command unrecognized\r\n")
+    else:
+        return SMTPResponse(SMTPCommand.RCPT, command[1], SMTPResponseCode.OK, b"250 OK\r\n")
+
+
+def handle_data_command(command: list[str]):
+    if len(command) != 1:
+        return SMTPResponse(SMTPCommand.ERROR, "", SMTPResponseCode.PARAMETER_ERROR, b"555 Parameter error, command unrecognized\r\n")
+    else:
+        return SMTPResponse(SMTPCommand.DATA, "", SMTPResponseCode.OK, b"354 Start mail input; end with <CRLF>.<CRLF>\r\n")
+    
+
+def handle_quit_command(command: list[str]):
+    if len(command) != 1:
+        return SMTPResponse(SMTPCommand.ERROR, "", SMTPResponseCode.PARAMETER_ERROR, b"555 Parameter error, command unrecognized\r\n")
+    else:
+        return SMTPResponse(SMTPCommand.QUIT, "", SMTPResponseCode.CLOSING_CONNECTION, b"221 Closing connection\r\n")
+    
+def handle_noop_command(command: list[str]):
+    if len(command) != 1:
+        return SMTPResponse(SMTPCommand.ERROR, "", SMTPResponseCode.PARAMETER_ERROR, b"555 Parameter error, command unrecognized\r\n")
+    else:
+        return SMTPResponse(SMTPCommand.NOOP, "", SMTPResponseCode.OK, b"250 OK\r\n")
+    
+
+def handle_rset_command(command: list[str]):
+    if len(command) != 1:
+        return SMTPResponse(SMTPCommand.ERROR, "", SMTPResponseCode.PARAMETER_ERROR, b"555 Parameter error, command unrecognized\r\n")
+    else:
+        return SMTPResponse(SMTPCommand.RSET, "", SMTPResponseCode.OK, b"250 OK\r\n")
+    
+
+def handle_data(mail: Mail, connection: socket.socket, interpreted: SMTPResponse):
+    mail.content = ""
+    connection.send(interpreted.response)
+    try:
+        while True:
+            print("Waiting for data")
+            data = connection.recv(1024).decode()
+            mail.content += data
+            print("Received data: ", data)
+            print("Mail content = ", mail.content)
+            if data == ".\r\n":
+                connection.sendall(b"250 OK\r\n")
+                break
+        print(f"Mail from {mail.sender} to {mail.recipient} with content {mail.content}")
+    except Exception as e:
+        print("Unexpected error occurred ", e)
+        connection.send(b"500 Syntax error, command unrecognized\r\n")
+
+
+def interpret_command(command):
+    try:
+        command = command.decode().replace("\r\n", "").split(" ")
+
+        print("Interpreting command: ", command)
+
+        if command[0] == "HELO":
+            return handle_helo_command(command)
+        elif command[0] == "MAIL":
+            return handle_mail_command(command)
+        elif command[0] == "RCPT":
+            return handle_rcpt_command(command)
+        elif command[0] == "DATA":
+            return handle_data_command(command)
+        elif command[0] == "NOOP":
+            return handle_noop_command(command)
+        elif command[0] == "RSET":
+            return handle_rset_command(command)
+        elif command[0] == "QUIT":
+            return handle_quit_command(command)
+        else:
+            return SMTPResponse(SMTPCommand.ERROR, "", SMTPResponseCode.SYNTAX_ERROR, b"500 Syntax error, command unrecognized\r\n")
+    except Exception as e:
+        print("Unexpected error occurred ", e)
+        return SMTPResponse(SMTPCommand.ERROR, "", SMTPResponseCode.SYNTAX_ERROR, b"500 Syntax error, command unrecognized\r\n")
 
 
 def handle_smtp_connection(connection):
@@ -64,8 +146,21 @@ def handle_smtp_connection(connection):
     while True:
         data = connection.recv(1024)
         interpreted = interpret_command(data)
-        connection.send(interpreted.response)
+
+        if interpreted.responseCode == SMTPResponseCode.OK:
+            if interpreted.command == SMTPCommand.RCPT:
+                mail.recipient = interpreted.data
+            elif interpreted.command == SMTPCommand.MAIL:
+                mail.sender = interpreted.data
+            elif interpreted.command == SMTPCommand.DATA:
+                handle_data(mail, connection, interpreted)
+
+        if interpreted.responseCode != SMTPResponseCode.OK or interpreted.command != SMTPCommand.DATA:
+            print("Sending response: ", interpreted.response)
+            connection.send(interpreted.response)
+
         if interpreted.responseCode == SMTPResponseCode.CLOSING_CONNECTION:
+            print("Closing connection")
             connection.close()
             break
 
@@ -77,16 +172,19 @@ def start_email_server():
 
         print(f"Server is listening on {IP}:{PORT}")
 
-        while True:
-            try:
+        try:
+            while True:
+                print("Waiting for connection")
                 connection, address = server.accept()
                 print(f"Connection with {address} has been established")
                 handle_smtp_connection(connection)
-            except KeyboardInterrupt:
-                print("Server is shutting down")
-                exit(0)
-            except Exception as e:
-                print("Unexpected error occurred ", e)
+        except KeyboardInterrupt:
+            print("Server is shutting down")
+            server.shutdown(socket.SHUT_RDWR)
+            server.close()
+            exit(0)
+        except Exception as e:
+            print("Unexpected error occurred ", e)
 
 if __name__ == "__main__":
     start_email_server()
--- Lab06/Zad10_client.py
+++ Lab06/Zad10_client.py
@@ -1,7 +1,112 @@
 import socket
+from enum import Enum
 
 IP = "127.0.0.1"
 PORT = 1588
+
+class SMTPCommand(Enum):
+    HELO = "HELO"
+    MAIL = "MAIL FROM"
+    RCPT = "RCPT TO"
+    DATA = "DATA"
+    NOOP = "NOOP"
+    RSET = "RSET"
+    QUIT = "QUIT"
+
+
+def map_id_to_command(id: int) -> SMTPCommand:
+    match id:
+        case 0: return SMTPCommand.HELO
+        case 1: return SMTPCommand.MAIL
+        case 2: return SMTPCommand.RCPT
+        case 3: return SMTPCommand.DATA
+        case 4: return SMTPCommand.NOOP
+        case 5: return SMTPCommand.RSET
+        case 6: return SMTPCommand.QUIT
+        case _: return None
+
+
+def send_with_response(message: str, connection: socket.socket) -> str:
+    try:
+        print(f"Sending message: {message}")
+        connection.sendall(f"{message}\r\n".encode("utf-8"))
+        data = connection.recv(4096).decode()
+        print(f"Received: {data}")
+        return data
+    except Exception as e:
+        print("Unexpected error occurred ", e)
+        return None
+    
+
+def send_without_response(message: str, connection: socket.socket):
+    try:
+        print(f"Sending message: {message}")
+        connection.sendall(f"{message}\r\n".encode("utf-8"))
+    except Exception as e:
+        print("Unexpected error occurred ", e)
+        return None
+    
+
+def get_command() -> SMTPCommand:
+    print("Select command from available: ")
+    for (idx, command) in enumerate(SMTPCommand):
+        print(f"{idx + 1}. {command.value}")
+
+    while True:
+        command = input("Enter command: ")
+        mapped = map_id_to_command(int(command) - 1)
+        if mapped:
+            return mapped
+        else:
+            print("Invalid command")
+
+
+def handle_command(connection: socket.socket):
+    while True:
+        command = get_command()
+        
+        if command in [SMTPCommand.HELO, SMTPCommand.RCPT, SMTPCommand.MAIL]:
+            parameter = input("Enter parameter: ")
+            message = f"{command.value} {parameter}"
+
+            response = send_with_response(message, connection)
+            if response and response.startswith("250"):
+                print("Message sent")
+            else:
+                print("Unexpected response from server while trying to send message")
+        elif command in [SMTPCommand.NOOP, SMTPCommand.RSET]:
+            response = send_with_response(command.value, connection)
+            if response and response.startswith("250"):
+                print("Message sent")
+            else:
+                print("Unexpected response from server while trying to send message")
+        elif command == SMTPCommand.QUIT:
+            print("Sending QUIT message...")
+            response = send_with_response(command.value, connection)
+            if response and response.startswith("221"):
+                print("Closing connection")
+                connection.close()
+                break
+            else:
+                print("Unexpected response from server while trying to QUIT")
+                continue
+        elif command == SMTPCommand.DATA:
+            print("Sending DATA message...")
+            response = send_with_response(command.value, connection)
+            if response and response.startswith("354"):
+                while True:
+                    message = input("Enter message: ")
+                    if message == ".":
+                        send_without_response(message, connection)
+                        print("End of message sent")
+                        break
+                    send_without_response(message, connection)
+
+                continue
+            else:
+                print("Unexpected response from server while trying to send DATA")
+                continue
+    
 
 
 def send_message():
@@ -11,14 +116,7 @@ def send_message():
             data = client.recv(4096)
             print("Received: ", data.decode())
 
-            while True:
-                message = input("Enter message: ")
-
-                print("Sending message...")
-                client.sendall(message.encode("utf-8"))
-                data = client.recv(4096)
-                print("Received: ", data.decode())
-
+            handle_command(client)
         except TimeoutError:
             print("Server is not responding")
         except InterruptedError:
